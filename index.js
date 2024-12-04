4c3f63b72f73966043cb0cda31f1a6d44e512fff
kr-sharma28 <krishnapurohit417400@gmail.com>
2024-12-04 23:05:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function isPrime(num){
 }
 console.log(isPrime(11)); //true
 console.log(isprime(15)); //false
+
+function isEven(num){
+    return num%2===0;
+}
+console.log(isEven(10)); // true
+console.log(isEven(11)); //false
